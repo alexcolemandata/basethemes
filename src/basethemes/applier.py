461b367fa7bfb7c92080c6561519b06c8316b265
alexcolemandata <coleman.g.alex@gmail.com>
2024-12-04 14:41:29 +1100

    add kitty theme applier for base16 themes

--- src/basethemes/applier.py
+++ src/basethemes/applier.py
@@ -1,11 +1,12 @@
 """Tools for applying a theme to various applications"""
+from __future__ import annotations
 
 from pathlib import Path
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import partial
 
-from basethemes.terminal_colors import TerminalColor, TerminalColors
+from basethemes.terminal_colors import TerminalColor, TerminalColors, Color
 
 
 DOT_CONFIG = Path("/Users/alex/.config")
@@ -32,6 +33,12 @@ class KittyColorMapping:
     inactive_tab_foreground: TerminalColor
     tab_bar_background: TerminalColor
 
+    def map_terminal_colors(self, terminal_colors: TerminalColors) -> dict[str, Color]:
+        return {
+            name: terminal_colors[terminal_color]
+            for name, terminal_color in self.__dict__.items()
+        }
+
 
 basic_mapping = KittyColorMapping(
     background=TerminalColor.COLOR_0,
@@ -56,21 +63,46 @@ class KittySetting:
     line_no: int
     is_commented_out: bool
     value: str
+    raw_line: str
+
+    def make_updated_setting(
+        self, is_commented_out: bool | None = None, value: str | None = None
+    ) -> KittySetting:
+        if is_commented_out is None:
+            is_commented_out = self.is_commented_out
+
+        if value is None:
+            value = self.value
+
+        return type(self)(
+            name=self.name,
+            line_no=self.line_no,
+            is_commented_out=is_commented_out,
+            value=value or value,
+            raw_line=self.raw_line,
+        )
+
+    @property
+    def formatted_line(self) -> str:
+        comment = "# " if self.is_commented_out else ""
+        return f"{comment}{self.name} {self.value}\n"
 
 
 @dataclass
 class KittyTheme:
-    mapping: KittyColorMapping
     colors: TerminalColors
+    mapping: KittyColorMapping = field(default_factory=lambda: basic_mapping)
 
+    def to_settings(self) -> dict[str, Color]:
+        base_colors = self.colors.to_dict()
+        derived_colors = self.mapping.map_terminal_colors(self.colors)
 
-kitty_theme = partial(KittyTheme, mapping=basic_mapping)
+        return base_colors | derived_colors
 
 
 class KittyApplier(ThemeApplier):
     app_name = "kitty"
-    settings: list[KittySetting]
-    updated_settings: list[KittySetting]
+    settings: dict[int, KittySetting]
 
     def __init__(self, config_file: Path | str) -> None:
         config_file = Path(config_file)
@@ -79,42 +111,49 @@ class KittyApplier(ThemeApplier):
             raise FileNotFoundError(f"could not locate config file {config_file}")
 
         self.config_file = config_file
-        self.settings = self.read_config()
+        self.settings = self.parse_config()
 
-    def read_config(self) -> list[KittySetting]:
-        """Read the config, skipping over any lines marked with '#:'"""
+        return None
+
+    def read_config(self) -> list[str]:
         with open(self.config_file, "r") as f:
-            conf_lines = f.readlines()
+            lines = f.readlines()
 
-        settings: list[KittySetting] = []
-        for line_no, line in enumerate(conf_lines):
+        return lines
+
+    def parse_config(self) -> dict[int, KittySetting]:
+        """Read the config, skipping over any lines marked with '#:'"""
+
+        settings: dict[int, KittySetting] = dict()
+        for line_no, line in enumerate(self.read_config()):
             stripped = line.strip()
             if stripped.startswith("#:") or not stripped:
                 continue
 
-            is_commented_out = line.startswith("#")
-            line = line.lstrip(" #")
+            is_commented_out = stripped.startswith("#")
+            stripped = stripped.lstrip(" #")
 
-            name, _, value = line.partition(" ")
+            name, _, value = stripped.partition(" ")
 
             if name == "map":
                 # consider key mappings to have the keyboard shortcut as part of name
                 shortcut, _, value = value.partition(" ")
                 name = f"{name} {shortcut}"
 
-            settings.append(
-                KittySetting(
-                    name=name,
-                    line_no=line_no,
-                    is_commented_out=is_commented_out,
-                    value=value.strip(),
-                )
+            settings[line_no] = KittySetting(
+                name=name,
+                line_no=line_no,
+                is_commented_out=is_commented_out,
+                value=value.strip(),
+                raw_line=line,
             )
 
         return settings
 
-    def get_setting(self, name: str) -> KittySetting:
-        results = [setting for setting in self.settings if setting.name == name]
+    def get_setting_by_name(self, name: str) -> KittySetting:
+        results = [
+            setting for setting in self.settings.values() if setting.name == name
+        ]
 
         if not results:
             raise KeyError(f"Could not find setting {name}")
@@ -124,6 +163,30 @@ class KittyApplier(ThemeApplier):
 
         return results[0]
 
-    def updated_settings_from_theme(self, theme: KittyTheme) -> list[KittySetting]:
-        breakpoint()
-        # terminal colors are easy
+    def updated_settings_from_theme(self, theme: KittyTheme) -> dict[int, KittySetting]:
+        theme_settings = theme.to_settings()
+
+        result: dict[int, KittySetting] = dict()
+        for setting, color in theme_settings.items():
+            updated = self.get_setting_by_name(setting).make_updated_setting(
+                is_commented_out=False, value=str(color).lower()
+            )
+            result[updated.line_no] = updated
+
+        return result
+
+    def apply_theme(self, theme: KittyTheme) -> None:
+        updated_settings = self.updated_settings_from_theme(theme)
+
+        lines = []
+        for line_no, line in enumerate(self.read_config()):
+            if line_no in updated_settings:
+                line = updated_settings[line_no].formatted_line
+            lines.append(line)
+
+        with open(self.config_file, "w") as f:
+            f.writelines(lines)
+
+        print(f"wrote updated to {self.config_file}")
+
+        return None
